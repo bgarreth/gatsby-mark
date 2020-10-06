3c7ae12f73c2ac8a1d2354eada73a349859af0a0
marksteven <mark@outbackagency.com.au>
2020-10-06 13:37:46 +1100

    withAuthagainButnoother

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
+import Amplify, { Auth } from "aws-amplify"
+import awsConfig from "./src/aws-exports"
+Amplify.configure(awsConfig)
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbreact/dist/css/mdb.css'
+//import '@fortawesome/fontawesome-free/css/all.min.css'
+//import 'bootstrap-css-only/css/bootstrap.min.css'
+//import 'mdbreact/dist/css/mdb.css'
 
